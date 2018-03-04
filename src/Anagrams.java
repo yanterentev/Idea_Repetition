5b82be570945c9299aefbfeb4e234d3b693a7982
yanterentev <yanterentev@gmail.com>
2018-03-04 17:29:49 +0300

    Java Anagrams completed

--- src/Anagrams.java
+++ src/Anagrams.java
@@ -4,7 +4,20 @@ public class Anagrams {
 
     private static boolean isAnagram(String a, String b) {
         int arr[] = new int[256];
-
+        a = a.toLowerCase();
+        b = b.toLowerCase();
+        for(int i = 0; i < a.length(); i++) {
+            arr[a.charAt(i)]++;
+        }
+        for(int i = 0; i < b.length(); i++) {
+            arr[b.charAt(i)]--;
+        }
+        for(int i = 0; i < 256; i++) {
+            if(arr[i] != 0) {
+                return false;
+            }
+        }
+        return true;
     }
 
     public static void main(String[] args) {
